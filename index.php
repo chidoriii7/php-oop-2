76e58fdc2b43ace958d9acc2e5eff3d1e805469a
chidoriii7 <muratore.danilo@yahoo.it>
2024-06-28 03:44:17 +0200

    php-oop-2: bozza eshop parte html

---
+++ index.php
@@ -0,0 +1,122 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>eShop Animali</title>
+
+    <!--inclusione bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!--inclusione css -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- inclusione cdn fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+</head>
+
+<body>
+
+    <header>
+        <h1 class="text-center py-5">eShop Animals   
+            <i class="fa-solid fa-paw"></i></h1>
+    </header>
+    <main>
+        <div class="container">
+            <!-- cat section -->
+            <section class="d-flex justify-content-center py-5">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="card" style="width: 18rem;">
+                            <img src="" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title">Cibo per Gatti</h5>
+                                <p class="card-text">product description</p>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">tipologia prodotto</li>
+                                <li class="list-group-item bg-lgray">Prezzo: €</li>
+                                <li class="list-group-item"><i class="fa-solid fa-cat"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="card" style="width: 18rem;">
+                            <img src="" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title">Giochi per Gatti</h5>
+                                <p class="card-text">product description</p>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">tipologia prodotto</li>
+                                <li class="list-group-item bg-lgray">Prezzo: €</li>
+                                <li class="list-group-item"><i class="fa-solid fa-cat"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- dog section -->
+            <section class="d-flex justify-content-center py-5">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="card" style="width: 18rem;">
+                            <img src="" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title">Cibo per Cani</h5>
+                                <p class="card-text">product description</p>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">tipologia prodotto</li>
+                                <li class="list-group-item bg-lgray">Prezzo: €</li>
+                                <li class="list-group-item"><i class="fa-solid fa-dog"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="card" style="width: 18rem;">
+                            <img src="" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title">Giochi per Cani</h5>
+                                <p class="card-text">product description</p>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">tipologia prodotto</li>
+                                <li class="list-group-item bg-lgray">Prezzo: €</li>
+                                <li class="list-group-item"><i class="fa-solid fa-dog"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- fish section -->
+            <section class="d-flex justify-content-center py-5">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card" style="width: 18rem;">
+                            <img src="" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title">Cibo per Pesci</h5>
+                                <p class="card-text">product description</p>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">tipologia prodotto</li>
+                                <li class="list-group-item bg-lgray">Prezzo: €</li>
+                                <li class="list-group-item"><i class="fa-solid fa-fish"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </main>
+</body>
+
+</html>
